24ec4e8bb680a130c44aa2f3737283cad6a94e4b
nickdeyoung <ndeyoung@sosacorp.com>
2015-09-28 15:51:53 -0700

    implementing api

--- filestore-client/src/main/scala/com/sosacorp/filestore/FilestoreClient.scala
+++ filestore-client/src/main/scala/com/sosacorp/filestore/FilestoreClient.scala
@@ -1,12 +1,17 @@
 package com.sosacorp.filestore
 
 
-import java.io.InputStream
+import java.io.{ByteArrayInputStream, InputStream}
+import java.nio.charset.StandardCharsets
 
+
+import dispatch._, Defaults._
 import com.sosacorp.logging.Logging
 import com.sosacorp.property.PropertyLoader
+import play.api.libs.json._
 
 import scala.concurrent.Future
+
 
 /**
  * Created by nickdeyoung on 9/28/15.
@@ -17,11 +22,35 @@ object FilestoreClient extends PropertyLoader with Logging {
   val filestorePort = getPropertyReq("filestore.port").toInt
 
   def read(path:String):Future[Either[String, InputStream]] = {
-    ???
+    logger.info(s"Attempting to read the file at $path from the file store")
+
+    val req = Http(host(filestoreHost, filestorePort) / "api" / "read" <<? List(("path", path)) OK as.String).either
+
+    val e = for ( e <- req.left ) yield {
+      val msg = s"Attempt to read $path failed with exception: ${e.getMessage}"
+      logger.error(msg)
+      msg
+    }
+
+    for ( s <- e.right ) yield {
+      new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8)) // WTF, HOW THE HELL DO I KNOW THE CHARSET OF THE FILE THAT WE SAVED ??? THIS IS A MAJOR FLAW, THE FILE STORE MUST SAVE THIS SOME HOW
+    }
   }
 
   def size(path:String):Future[Either[String, Long]] = {
-    ???
+    logger.info(s"Attempting to get size of the file at $path from the file store")
+
+    val req = Http(host(filestoreHost, filestorePort) / "api" / "size" <<? List(("path", path)) OK as.String).either
+
+    val e = for ( e <- req.left ) yield {
+      val msg = s"Attempt to get size of $path failed with exception: ${e.getMessage}"
+      logger.error(msg)
+      msg
+    }
+
+    for ( s <- e.right ) yield {
+      s.toInt
+    }
   }
 
   def write(source:InputStream, dest:String):Future[Either[String,Long]] = {
@@ -29,11 +58,45 @@ object FilestoreClient extends PropertyLoader with Logging {
   }
 
   def list(dir:String, prefixOpt:Option[String]):Future[Either[String, List[String]]] = {
-    ???
+    logger.info(s"Attempting to list the directory at $dir from the file store")
+
+    val req = Http(host(filestoreHost, filestorePort) / "api" / "list" <<? List(("path", dir), ("prefix", prefixOpt.getOrElse(""))) OK as.String).either
+
+    val e = for ( e <- req.left ) yield {
+      val msg = s"Attempt to list $dir failed with exception: ${e.getMessage}"
+      logger.error(msg)
+      msg
+    }
+
+    for (s <- e.right) yield {
+      val json = Json.parse(s)
+      json match {
+        case x:JsArray =>
+          x.value.map { v => v.toString()}.toList
+        case _ => Nil
+      }
+    }
   }
 
   def exists(path:String):Future[Either[String, Boolean]] = {
-    ???
+    logger.info(s"Attempting to see if file at $path exists in the file store")
+
+    val req = Http(host(filestoreHost, filestorePort) / "api" / "exists" <<? List(("path", path)) OK as.String).either
+
+    val e = for ( e <- req.left ) yield {
+      val msg = s"Attempting to see if file at $path exists in the file store failed with exception: ${e.getMessage}"
+      logger.error(msg)
+      msg
+    }
+
+    for (s <- e.right) yield {
+      val json = Json.parse(s)
+      json match {
+        case x:JsBoolean =>
+          x.value
+        case _ => false
+      }
+    }
   }
 
 }
--- filestore/app/filestore/controllers/api/FilestoreApi.scala
+++ filestore/app/filestore/controllers/api/FilestoreApi.scala
@@ -1,8 +1,66 @@
 package filestore.controllers.api
 
+import java.io.{InputStream, File, FileInputStream}
+
+import com.sosacorp.binder.Refs
+import com.sosacorp.content.file.FileStore
+import play.api.libs.Files.TemporaryFile
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.json.Json
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.io.Source
+
+
 /**
  * Created by nickdeyoung on 9/28/15.
  */
-object FilestoreApi {
+object FilestoreApi extends Controller {
+
+  lazy val fileStore = Refs.refTo[FileStore]
+
+  def read(path: String) = Action.async { implicit request => future {
+    fileStore.read(path).map { stream =>
+      new SimpleResult(header = ResponseHeader(OK, Map()), body = Enumerator.fromStream(stream))
+    }.getOrElse {
+      BadRequest(s"cannot load path $path")
+    }
+  }}
+
+  def size(path: String) = Action.async { implicit request => future {
+    lclRead(path)
+  }}
+
+  def write(dest: String) = Action.async { implicit request => future {
+    request.body match {
+      case AnyContentAsMultipartFormData(formData: MultipartFormData[TemporaryFile]) =>
+        formData.files.headOption.map { tmpFile =>
+          lclWrite(new FileInputStream(new File(tmpFile.ref.file.getAbsolutePath)), dest)
+          lclRead(dest)
+        }.getOrElse(BadRequest(s"cannot write file, no file to write $dest"))
+
+      case _ => BadRequest("Expected request of type multipart/form-data")
+    }
+  }}
+
+  def list(dir: String, prefixOpt: Option[String]) = Action.async { implicit request => future {
+    Ok(Json.toJson(fileStore.list(dir, prefixOpt)))
+  }}
+
+  def exists(path: String) = Action.async { implicit request => future {
+    Ok(Json.toJson(fileStore.exists(path)))
+  }}
+
+  private def lclWrite(stream:InputStream, path:String) = {
+    fileStore.write(stream, path)
+  }
+
+  private def lclRead(path:String) = {
+    fileStore.read(path).map { stream =>
+      Ok(Json.toJson(Source.fromInputStream(stream).size))
+    }.getOrElse(BadRequest(s"cannot write file $path"))
+  }
 
 }
